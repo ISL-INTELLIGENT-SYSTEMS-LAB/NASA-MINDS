924e806575890f3676869551deeb0116601c54ec
Taylor Brown <78773029+TaylorBrown96@users.noreply.github.com>
2024-02-02 20:27:08 -0500

    Add files via upload

--- NASA_MINDS/Inventory_Tracking_and_Managment/ShoppingListImport_Form.Designer.cs
+++ NASA_MINDS/Inventory_Tracking_and_Managment/ShoppingListImport_Form.Designer.cs
@@ -28,94 +28,193 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ShoppingListImport_Form));
             this.panel1 = new System.Windows.Forms.Panel();
-            this.Btn_Back = new System.Windows.Forms.Button();
-            this.Pb_Item = new System.Windows.Forms.PictureBox();
+            this.Btn_LoadItems = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.sqliteDataAccessBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.Item = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ItemName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ItemDescription = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SerialNum = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Condition = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.RNNum = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.RFID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Btn_Back = new System.Windows.Forms.Button();
+            this.PB_Item = new System.Windows.Forms.PictureBox();
+            this.LB_FoundItems = new System.Windows.Forms.ListBox();
+            this.Btn_Locate = new System.Windows.Forms.Button();
+            this.Btn_Found = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.Pb_Item)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.sqliteDataAccessBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_Item)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.Btn_Found);
+            this.panel1.Controls.Add(this.Btn_Locate);
+            this.panel1.Controls.Add(this.LB_FoundItems);
+            this.panel1.Controls.Add(this.PB_Item);
+            this.panel1.Controls.Add(this.Btn_LoadItems);
             this.panel1.Controls.Add(this.dataGridView1);
             this.panel1.Controls.Add(this.Btn_Back);
-            this.panel1.Controls.Add(this.Pb_Item);
-            this.panel1.Location = new System.Drawing.Point(35, 36);
+            this.panel1.Location = new System.Drawing.Point(15, 14);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1174, 602);
+            this.panel1.Size = new System.Drawing.Size(2081, 942);
             this.panel1.TabIndex = 0;
+            // 
+            // Btn_LoadItems
+            // 
+            this.Btn_LoadItems.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_LoadItems.Location = new System.Drawing.Point(107, 4);
+            this.Btn_LoadItems.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Btn_LoadItems.Name = "Btn_LoadItems";
+            this.Btn_LoadItems.Size = new System.Drawing.Size(223, 37);
+            this.Btn_LoadItems.TabIndex = 24;
+            this.Btn_LoadItems.Text = "Import Shopping List";
+            this.Btn_LoadItems.UseVisualStyleBackColor = true;
+            this.Btn_LoadItems.Click += new System.EventHandler(this.Btn_LoadItems_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ItemName,
+            this.ItemDescription,
+            this.SerialNum,
+            this.Condition,
+            this.RNNum,
+            this.RFID});
+            this.dataGridView1.Location = new System.Drawing.Point(4, 47);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(1105, 876);
+            this.dataGridView1.TabIndex = 23;
+            // 
+            // ItemName
+            // 
+            this.ItemName.HeaderText = "Item Name";
+            this.ItemName.MinimumWidth = 6;
+            this.ItemName.Name = "ItemName";
+            this.ItemName.ReadOnly = true;
+            this.ItemName.Width = 125;
+            // 
+            // ItemDescription
+            // 
+            this.ItemDescription.HeaderText = "Description";
+            this.ItemDescription.MinimumWidth = 6;
+            this.ItemDescription.Name = "ItemDescription";
+            this.ItemDescription.ReadOnly = true;
+            this.ItemDescription.Width = 125;
+            // 
+            // SerialNum
+            // 
+            this.SerialNum.HeaderText = "Searial #";
+            this.SerialNum.MinimumWidth = 6;
+            this.SerialNum.Name = "SerialNum";
+            this.SerialNum.ReadOnly = true;
+            this.SerialNum.Width = 125;
+            // 
+            // Condition
+            // 
+            this.Condition.HeaderText = "Condition";
+            this.Condition.MinimumWidth = 6;
+            this.Condition.Name = "Condition";
+            this.Condition.ReadOnly = true;
+            this.Condition.Width = 125;
+            // 
+            // RNNum
+            // 
+            this.RNNum.HeaderText = "RN #";
+            this.RNNum.MinimumWidth = 6;
+            this.RNNum.Name = "RNNum";
+            this.RNNum.ReadOnly = true;
+            this.RNNum.Width = 125;
+            // 
+            // RFID
+            // 
+            this.RFID.HeaderText = "RFID#";
+            this.RFID.MinimumWidth = 6;
+            this.RFID.Name = "RFID";
+            this.RFID.ReadOnly = true;
+            this.RFID.Width = 125;
             // 
             // Btn_Back
             // 
             this.Btn_Back.BackColor = System.Drawing.Color.Red;
             this.Btn_Back.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Btn_Back.Location = new System.Drawing.Point(4, 5);
-            this.Btn_Back.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Btn_Back.Location = new System.Drawing.Point(4, 4);
+            this.Btn_Back.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Btn_Back.Name = "Btn_Back";
-            this.Btn_Back.Size = new System.Drawing.Size(108, 46);
+            this.Btn_Back.Size = new System.Drawing.Size(96, 37);
             this.Btn_Back.TabIndex = 22;
             this.Btn_Back.Text = "Back";
             this.Btn_Back.UseVisualStyleBackColor = false;
             this.Btn_Back.Click += new System.EventHandler(this.Btn_Back_Click);
             // 
-            // Pb_Item
+            // PB_Item
             // 
-            this.Pb_Item.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Pb_Item.BackgroundImage")));
-            this.Pb_Item.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.Pb_Item.Location = new System.Drawing.Point(445, 59);
-            this.Pb_Item.Name = "Pb_Item";
-            this.Pb_Item.Size = new System.Drawing.Size(175, 220);
-            this.Pb_Item.TabIndex = 0;
-            this.Pb_Item.TabStop = false;
+            this.PB_Item.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.PB_Item.Location = new System.Drawing.Point(1465, 47);
+            this.PB_Item.Name = "PB_Item";
+            this.PB_Item.Size = new System.Drawing.Size(488, 386);
+            this.PB_Item.TabIndex = 25;
+            this.PB_Item.TabStop = false;
             // 
-            // dataGridView1
+            // LB_FoundItems
             // 
-            this.dataGridView1.AllowUserToDeleteRows = false;
-            this.dataGridView1.AutoGenerateColumns = false;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Item});
-            this.dataGridView1.DataSource = this.sqliteDataAccessBindingSource;
-            this.dataGridView1.Location = new System.Drawing.Point(4, 59);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 62;
-            this.dataGridView1.RowTemplate.Height = 28;
-            this.dataGridView1.Size = new System.Drawing.Size(435, 529);
-            this.dataGridView1.TabIndex = 23;
+            this.LB_FoundItems.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LB_FoundItems.FormattingEnabled = true;
+            this.LB_FoundItems.ItemHeight = 54;
+            this.LB_FoundItems.Location = new System.Drawing.Point(1115, 439);
+            this.LB_FoundItems.Name = "LB_FoundItems";
+            this.LB_FoundItems.Size = new System.Drawing.Size(947, 544);
+            this.LB_FoundItems.TabIndex = 26;
             // 
-            // sqliteDataAccessBindingSource
+            // Btn_Locate
             // 
-            this.sqliteDataAccessBindingSource.DataSource = typeof(NASA_MINDS_Library.sqliteDataAccess);
+            this.Btn_Locate.Location = new System.Drawing.Point(1132, 59);
+            this.Btn_Locate.Name = "Btn_Locate";
+            this.Btn_Locate.Size = new System.Drawing.Size(312, 161);
+            this.Btn_Locate.TabIndex = 27;
+            this.Btn_Locate.Text = "Locate Item";
+            this.Btn_Locate.UseVisualStyleBackColor = true;
+            this.Btn_Locate.Click += new System.EventHandler(this.Btn_Locate_Click);
             // 
-            // Item
+            // Btn_Found
             // 
-            this.Item.HeaderText = "Item name";
-            this.Item.MinimumWidth = 8;
-            this.Item.Name = "Item";
-            this.Item.Width = 150;
+            this.Btn_Found.Location = new System.Drawing.Point(1132, 249);
+            this.Btn_Found.Name = "Btn_Found";
+            this.Btn_Found.Size = new System.Drawing.Size(312, 165);
+            this.Btn_Found.TabIndex = 28;
+            this.Btn_Found.Text = "Item Found";
+            this.Btn_Found.UseVisualStyleBackColor = true;
+            this.Btn_Found.Click += new System.EventHandler(this.Btn_Found_Click);
             // 
             // ShoppingListImport_Form
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(1243, 681);
+            this.ClientSize = new System.Drawing.Size(2111, 969);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "ShoppingListImport_Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "ShoppingListImport_Form";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.panel1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.Pb_Item)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.sqliteDataAccessBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_Item)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -123,10 +222,18 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.PictureBox Pb_Item;
         private System.Windows.Forms.Button Btn_Back;
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Item;
-        private System.Windows.Forms.BindingSource sqliteDataAccessBindingSource;
+        private System.Windows.Forms.Button Btn_LoadItems;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ItemName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ItemDescription;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SerialNum;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Condition;
+        private System.Windows.Forms.DataGridViewTextBoxColumn RNNum;
+        private System.Windows.Forms.DataGridViewTextBoxColumn RFID;
+        private System.Windows.Forms.PictureBox PB_Item;
+        private System.Windows.Forms.Button Btn_Locate;
+        private System.Windows.Forms.ListBox LB_FoundItems;
+        private System.Windows.Forms.Button Btn_Found;
     }
 }
--- NASA_MINDS/Inventory_Tracking_and_Managment/ShoppingListImport_Form.cs
+++ NASA_MINDS/Inventory_Tracking_and_Managment/ShoppingListImport_Form.cs
@@ -1,17 +1,16 @@
-﻿using System;
+﻿using NASA_MINDS_Library;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+
 
 namespace Inventory_Tracking_and_Managment
 {
     public partial class ShoppingListImport_Form : Form
     {
+        // Create a list of items from the database
+        private List<Item> items = new List<Item>();
         public ShoppingListImport_Form()
         {
             InitializeComponent();
@@ -19,10 +18,87 @@ namespace Inventory_Tracking_and_Managment
 
         private void Btn_Back_Click(object sender, EventArgs e)
         {
+            // Close the current form and open the ShoppingList_Form
             Hide();
             var ShoppingList_Form = new ShoppingList_Form();
             ShoppingList_Form.Closed += (s, args) => Close();
             ShoppingList_Form.Show();
+        }
+
+        private void Btn_LoadItems_Click(object sender, EventArgs e)
+        {
+            // Populate the list of items from the database
+            items.Add(sqliteDataAccess.PopulateData("1"));
+            items.Add(sqliteDataAccess.PopulateData("2"));
+            items.Add(sqliteDataAccess.PopulateData("3"));
+
+            foreach (Item item in items)
+            {
+                AddRows(item);
+            }
+        }
+
+        private void AddRows(Item item)
+        {
+            // Check the condition of the item and convert it to a string
+            string condition;
+            if (item.Condition == 0)
+            {
+                condition = "New";
+            }
+            else if (item.Condition == 1)
+            {
+                condition = "Good";
+            }
+            else if (item.Condition == 2)
+            {
+                condition = "Needs repair";
+            }
+            else if (item.Condition == 3)
+            {
+                condition = "Broken";
+            }
+            else
+            {
+                condition = "Unknown";
+            }
+
+            // Add the item to the datagridview
+            dataGridView1.Rows.Add(item.ItemName, item.ItemDescription, item.SerialNum, condition, item.RNNum, item.RFID);
+        }
+
+        private void Btn_Found_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Get selected row index from datagridview and add the items name to the LB_FoundItems listbox
+                int index = dataGridView1.CurrentCell.RowIndex;
+                string itemName = dataGridView1.Rows[index].Cells[0].Value.ToString();
+                dataGridView1.Rows.RemoveAt(index);
+                LB_FoundItems.Items.Add(itemName);
+            }
+            catch
+            {
+                return;
+            }
+        }
+
+        private void Btn_Locate_Click(object sender, EventArgs e)
+        {
+            // Get current working directory and add the image location to the LB_FoundItems listbox
+            string currentDir = Environment.CurrentDirectory;
+
+            try
+            {
+                // Get selected row index from the datagridview and update image in the PB_Item picturebox
+                int index = dataGridView1.CurrentCell.RowIndex;
+                string imageLocation = currentDir + "/images/" + items.Find(x => x.ItemName == dataGridView1.Rows[index].Cells[0].Value.ToString()).Image_loc;
+                PB_Item.BackgroundImage = new Bitmap(imageLocation);
+            }
+            catch
+            {
+                return;
+            }
         }
     }
 }
