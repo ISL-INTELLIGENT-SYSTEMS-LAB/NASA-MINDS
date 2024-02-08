cd9888f3c3fdd7f51a5052145691747a0b0225b4
Luis <134240596+LuisHernandez9@users.noreply.github.com>
2024-02-08 01:17:14 -0500

    Add files via upload

--- NASA_MINDS/Inventory_Tracking_and_Managment/FindItems_Form.Designer.cs
+++ NASA_MINDS/Inventory_Tracking_and_Managment/FindItems_Form.Designer.cs
@@ -31,6 +31,12 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FindItems_Form));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.L_Condition = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.RTB_Description = new System.Windows.Forms.RichTextBox();
+            this.L_SerialNum = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.CB_ItemSearch = new System.Windows.Forms.ComboBox();
             this.Btn_Back = new System.Windows.Forms.Button();
             this.L_TagId = new System.Windows.Forms.Label();
             this.L_RO = new System.Windows.Forms.Label();
@@ -40,12 +46,6 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
             this.Pb_Item = new System.Windows.Forms.PictureBox();
-            this.CB_ItemSearch = new System.Windows.Forms.ComboBox();
-            this.L_SerialNum = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.RTB_Description = new System.Windows.Forms.RichTextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.L_Condition = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.Pb_Item)).BeginInit();
@@ -56,9 +56,10 @@
             this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.pictureBox1.Location = new System.Drawing.Point(1296, 435);
+            this.pictureBox1.Location = new System.Drawing.Point(1728, 535);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(153, 143);
+            this.pictureBox1.Size = new System.Drawing.Size(204, 176);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
@@ -81,18 +82,86 @@
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.label9);
             this.panel1.Controls.Add(this.Pb_Item);
-            this.panel1.Location = new System.Drawing.Point(100, -1);
+            this.panel1.Location = new System.Drawing.Point(133, -1);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1192, 583);
+            this.panel1.Size = new System.Drawing.Size(1589, 718);
             this.panel1.TabIndex = 1;
+            // 
+            // L_Condition
+            // 
+            this.L_Condition.AutoSize = true;
+            this.L_Condition.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.L_Condition.Location = new System.Drawing.Point(892, 385);
+            this.L_Condition.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.L_Condition.Name = "L_Condition";
+            this.L_Condition.Size = new System.Drawing.Size(0, 46);
+            this.L_Condition.TabIndex = 27;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(671, 384);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(200, 44);
+            this.label3.TabIndex = 25;
+            this.label3.Text = "Condition:";
+            // 
+            // RTB_Description
+            // 
+            this.RTB_Description.Enabled = false;
+            this.RTB_Description.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RTB_Description.Location = new System.Drawing.Point(929, 224);
+            this.RTB_Description.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.RTB_Description.Name = "RTB_Description";
+            this.RTB_Description.Size = new System.Drawing.Size(563, 147);
+            this.RTB_Description.TabIndex = 24;
+            this.RTB_Description.Text = "";
+            // 
+            // L_SerialNum
+            // 
+            this.L_SerialNum.AutoSize = true;
+            this.L_SerialNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.L_SerialNum.Location = new System.Drawing.Point(856, 427);
+            this.L_SerialNum.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.L_SerialNum.Name = "L_SerialNum";
+            this.L_SerialNum.Size = new System.Drawing.Size(0, 46);
+            this.L_SerialNum.TabIndex = 23;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("MS UI Gothic", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.Black;
+            this.label7.Location = new System.Drawing.Point(671, 427);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(165, 44);
+            this.label7.TabIndex = 22;
+            this.label7.Text = "Serial #:";
+            // 
+            // CB_ItemSearch
+            // 
+            this.CB_ItemSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CB_ItemSearch.FormattingEnabled = true;
+            this.CB_ItemSearch.Location = new System.Drawing.Point(472, 54);
+            this.CB_ItemSearch.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.CB_ItemSearch.Name = "CB_ItemSearch";
+            this.CB_ItemSearch.Size = new System.Drawing.Size(596, 33);
+            this.CB_ItemSearch.TabIndex = 21;
+            this.CB_ItemSearch.SelectedIndexChanged += new System.EventHandler(this.CB_ItemSearch_SelectedIndexChanged);
             // 
             // Btn_Back
             // 
             this.Btn_Back.BackColor = System.Drawing.Color.Red;
             this.Btn_Back.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Btn_Back.Location = new System.Drawing.Point(19, 14);
+            this.Btn_Back.Location = new System.Drawing.Point(25, 17);
+            this.Btn_Back.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Btn_Back.Name = "Btn_Back";
-            this.Btn_Back.Size = new System.Drawing.Size(72, 30);
+            this.Btn_Back.Size = new System.Drawing.Size(96, 37);
             this.Btn_Back.TabIndex = 20;
             this.Btn_Back.Text = "Back";
             this.Btn_Back.UseVisualStyleBackColor = false;
@@ -102,27 +171,30 @@
             // 
             this.L_TagId.AutoSize = true;
             this.L_TagId.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.L_TagId.Location = new System.Drawing.Point(628, 419);
+            this.L_TagId.Location = new System.Drawing.Point(837, 516);
+            this.L_TagId.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.L_TagId.Name = "L_TagId";
-            this.L_TagId.Size = new System.Drawing.Size(0, 37);
+            this.L_TagId.Size = new System.Drawing.Size(0, 46);
             this.L_TagId.TabIndex = 18;
             // 
             // L_RO
             // 
             this.L_RO.AutoSize = true;
             this.L_RO.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.L_RO.Location = new System.Drawing.Point(606, 382);
+            this.L_RO.Location = new System.Drawing.Point(808, 470);
+            this.L_RO.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.L_RO.Name = "L_RO";
-            this.L_RO.Size = new System.Drawing.Size(0, 37);
+            this.L_RO.Size = new System.Drawing.Size(0, 46);
             this.L_RO.TabIndex = 17;
             // 
             // L_Name
             // 
             this.L_Name.AutoSize = true;
             this.L_Name.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.L_Name.Location = new System.Drawing.Point(614, 145);
+            this.L_Name.Location = new System.Drawing.Point(819, 178);
+            this.L_Name.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.L_Name.Name = "L_Name";
-            this.L_Name.Size = new System.Drawing.Size(0, 37);
+            this.L_Name.Size = new System.Drawing.Size(0, 46);
             this.L_Name.TabIndex = 13;
             // 
             // label6
@@ -130,9 +202,10 @@
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("MS UI Gothic", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.ForeColor = System.Drawing.Color.Black;
-            this.label6.Location = new System.Drawing.Point(503, 382);
+            this.label6.Location = new System.Drawing.Point(671, 470);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(98, 35);
+            this.label6.Size = new System.Drawing.Size(122, 44);
             this.label6.TabIndex = 12;
             this.label6.Text = "RO #:";
             // 
@@ -141,9 +214,10 @@
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("MS UI Gothic", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.Black;
-            this.label5.Location = new System.Drawing.Point(503, 419);
+            this.label5.Location = new System.Drawing.Point(671, 516);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(119, 35);
+            this.label5.Size = new System.Drawing.Size(148, 44);
             this.label5.TabIndex = 11;
             this.label5.Text = "Tag ID:";
             // 
@@ -152,9 +226,10 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("MS UI Gothic", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Black;
-            this.label1.Location = new System.Drawing.Point(503, 182);
+            this.label1.Location = new System.Drawing.Point(671, 224);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(188, 35);
+            this.label1.Size = new System.Drawing.Size(235, 44);
             this.label1.TabIndex = 7;
             this.label1.Text = "Description:";
             // 
@@ -163,9 +238,10 @@
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("MS UI Gothic", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label9.ForeColor = System.Drawing.Color.Black;
-            this.label9.Location = new System.Drawing.Point(503, 144);
+            this.label9.Location = new System.Drawing.Point(671, 177);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(105, 35);
+            this.label9.Size = new System.Drawing.Size(131, 44);
             this.label9.TabIndex = 6;
             this.label9.Text = "Name:";
             // 
@@ -174,81 +250,26 @@
             this.Pb_Item.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Pb_Item.BackgroundImage")));
             this.Pb_Item.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.Pb_Item.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.Pb_Item.Location = new System.Drawing.Point(19, 108);
+            this.Pb_Item.Location = new System.Drawing.Point(25, 133);
+            this.Pb_Item.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Pb_Item.Name = "Pb_Item";
-            this.Pb_Item.Size = new System.Drawing.Size(460, 389);
+            this.Pb_Item.Size = new System.Drawing.Size(613, 478);
             this.Pb_Item.TabIndex = 0;
             this.Pb_Item.TabStop = false;
             // 
-            // CB_ItemSearch
-            // 
-            this.CB_ItemSearch.FormattingEnabled = true;
-            this.CB_ItemSearch.Location = new System.Drawing.Point(354, 44);
-            this.CB_ItemSearch.Name = "CB_ItemSearch";
-            this.CB_ItemSearch.Size = new System.Drawing.Size(448, 21);
-            this.CB_ItemSearch.TabIndex = 21;
-            this.CB_ItemSearch.SelectedIndexChanged += new System.EventHandler(this.CB_ItemSearch_SelectedIndexChanged);
-            // 
-            // L_SerialNum
-            // 
-            this.L_SerialNum.AutoSize = true;
-            this.L_SerialNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.L_SerialNum.Location = new System.Drawing.Point(642, 347);
-            this.L_SerialNum.Name = "L_SerialNum";
-            this.L_SerialNum.Size = new System.Drawing.Size(0, 37);
-            this.L_SerialNum.TabIndex = 23;
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("MS UI Gothic", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.ForeColor = System.Drawing.Color.Black;
-            this.label7.Location = new System.Drawing.Point(503, 347);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(133, 35);
-            this.label7.TabIndex = 22;
-            this.label7.Text = "Serial #:";
-            // 
-            // RTB_Description
-            // 
-            this.RTB_Description.Location = new System.Drawing.Point(697, 182);
-            this.RTB_Description.Name = "RTB_Description";
-            this.RTB_Description.Size = new System.Drawing.Size(423, 120);
-            this.RTB_Description.TabIndex = 24;
-            this.RTB_Description.Text = "";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.Color.Black;
-            this.label3.Location = new System.Drawing.Point(503, 312);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(160, 35);
-            this.label3.TabIndex = 25;
-            this.label3.Text = "Condition:";
-            // 
-            // L_Condition
-            // 
-            this.L_Condition.AutoSize = true;
-            this.L_Condition.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.L_Condition.Location = new System.Drawing.Point(669, 313);
-            this.L_Condition.Name = "L_Condition";
-            this.L_Condition.Size = new System.Drawing.Size(0, 37);
-            this.L_Condition.TabIndex = 27;
-            // 
             // FindItems_Form
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.ClientSize = new System.Drawing.Size(1245, 590);
+            this.ClientSize = new System.Drawing.Size(1660, 726);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.pictureBox1);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FindItems_Form";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FindItems_Form";
